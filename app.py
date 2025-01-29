03089ef39275976a81f666c13aeb9d73027c5bf8
keean <kvidyart@students.cs.ubc.ca>
2025-01-29 01:50:11 -0800

    Documentation for row_reduce function

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ import numpy as np
 
 app = Flask(__name__)
 
+#Uses Gaussian elimination on a 3x3 user-inputted matrix
+#to reduce to Reduced Row Echelon Form
 def row_reduce(matrix):
+    
+    #Specify NumPy array
     matrix = np.array(matrix, dtype=float)
     rows, cols = matrix.shape
 
+    #Row reduction
     for i in range(min(rows, cols)):
         if matrix[i][i] != 0:
             matrix[i] = matrix[i] / matrix[i][i]
